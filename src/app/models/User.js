f5bf5648dfc8eba142983ad766fa2e332f2cc0dc
rauzola <raul_sigoli@hotmail.com>
2020-04-07 03:03:42 -0300

    02   Avatar do usuário

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,5 +1,5 @@
-import Sequelize, { Model } from 'sequelize';
-import bcrypt from 'bcryptjs';
+import Sequelize, { Model } from "sequelize";
+import bcrypt from "bcryptjs";
 
 class User extends Model {
   static init(sequelize) {
@@ -16,7 +16,7 @@ class User extends Model {
       }
     );
 
-    this.addHook('beforeSave', async user => {
+    this.addHook("beforeSave", async (user) => {
       if (user.password) {
         user.password_hash = await bcrypt.hash(user.password, 8);
       }
@@ -25,8 +25,13 @@ class User extends Model {
     return this;
   }
 
+  static associate(models) {
+    this.belongsTo(models.File, { foreignKey: "avatar_id", as: "avatar" });
+  }
+
   checkPassword(password) {
     return bcrypt.compare(password, this.password_hash);
   }
 }
+
 export default User;
