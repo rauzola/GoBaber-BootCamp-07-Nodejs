bd98877bdbb14c60f9436126c271b03d9c75848f
rauzola <raul_sigoli@hotmail.com>
2020-04-07 05:55:26 -0300

    09   Listando agenda do prestador

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,7 @@ import UserController from "./app/controllers/UserController";
 import SessionController from "./app/controllers/SessionController";
 import FileController from "./app/controllers/FileController";
 import ProviderController from "./app/controllers/ProviderController";
+import ScheduleController from "./app/controllers/ScheduleController";
 
 import authMiddleware from "./app/middlewares/auth";
 import AppointmentController from "./app/controllers/AppointmentController";
@@ -24,6 +25,8 @@ routes.get("/providers", ProviderController.index);
 
 routes.get("/appointments", AppointmentController.index);
 routes.post("/appointments", AppointmentController.store);
+
+routes.get("/schedule", ScheduleController.index);
 
 routes.post("/files", upload.single("file"), FileController.store);
 
