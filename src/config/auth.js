018e4bf6c719f24c1053ee22060f02efdcd0b6ee
rauzola <raul_sigoli@hotmail.com>
2020-04-07 02:11:49 -0300

    modulo03

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: "f95f4d5bae104702db652353910315f2",
+  expiresIn: "7d",
+};
---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: "postgres",
+  host: "localhost",
+  username: "postgres",
+  password: "01110010",
+  database: "testando1",
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
