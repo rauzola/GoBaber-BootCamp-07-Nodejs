fd8c2d78a7442e6672daccb8ea8c1e4b79e6cc03
rauzola <raul_sigoli@hotmail.com>
2020-04-08 02:17:26 -0300

    12   Listando notificações do usuário

--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,7 @@ import SessionController from "./app/controllers/SessionController";
 import FileController from "./app/controllers/FileController";
 import ProviderController from "./app/controllers/ProviderController";
 import ScheduleController from "./app/controllers/ScheduleController";
+import NotificationController from "./app/controllers/NotificationController";
 
 import authMiddleware from "./app/middlewares/auth";
 import AppointmentController from "./app/controllers/AppointmentController";
@@ -27,6 +28,8 @@ routes.get("/appointments", AppointmentController.index);
 routes.post("/appointments", AppointmentController.store);
 
 routes.get("/schedule", ScheduleController.index);
+
+routes.get("/notifications", NotificationController.index);
 
 routes.post("/files", upload.single("file"), FileController.store);
 
